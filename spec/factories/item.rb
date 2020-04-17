6af5cd8031a7cca454e535c421296c59078d028d
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-17 15:12:57 -0600

    Add factory bot objects

---
+++ spec/factories/item.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item, class: Item do
+    name        { Faker::Commerce.product_name}
+    description { Faker::ChuckNorris.fact }
+    price       { Faker::Commerce.price}
+    image       { Faker::Avatar.image }
+    active?     {true}
+    inventory   { Faker::Number.between(from: 1, to: 500)}
+  end
+end
