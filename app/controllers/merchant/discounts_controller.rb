3c9eadc3bce1a149b05d183391e58885b58a8134
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-18 23:16:10 -0600

    Add new and create on discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,17 +1,34 @@
 class Merchant::DiscountsController < Merchant::BaseController
 
   def index
-    @discounts = current_user.merchant.discounts
+    @discounts = current_merchant.discounts
   end
 
   def show
     @discount = curret_discounts
   end
 
-  private
-
-  def curret_discounts
-    Discount.find(params[:id])
+  def new
+    @discount = Discount.new
   end
 
+  def create
+    merchant = current_merchant
+    discount = merchant.discounts.create(discount_params)
+
+    if discount.save
+      redirect_to merchant_discounts_path
+    else
+      generate_flash(discount)
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:name,
+                  :percent_off,
+                  :min_quantity)
+  end
 end
