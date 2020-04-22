43cbbc6ce98a5c49a7d1290d11453772275f5395
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-22 00:56:32 -0600

    Migrate to db

--- db/schema.rb
+++ db/schema.rb
@@ -38,16 +38,6 @@ ActiveRecord::Schema.define(version: 20200421204338) do
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.integer "zip"
-    t.integer "nickname", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "merchants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -99,15 +89,6 @@ ActiveRecord::Schema.define(version: 20200421204338) do
     t.index ["user_id"], name: "index_user_addresses_on_user_id"
   end
 
-  create_table "user_locations", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "location_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["location_id"], name: "index_user_locations_on_location_id"
-    t.index ["user_id"], name: "index_user_locations_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -126,7 +107,5 @@ ActiveRecord::Schema.define(version: 20200421204338) do
   add_foreign_key "orders", "users"
   add_foreign_key "reviews", "items"
   add_foreign_key "user_addresses", "users"
-  add_foreign_key "user_locations", "locations"
-  add_foreign_key "user_locations", "users"
   add_foreign_key "users", "merchants"
 end
