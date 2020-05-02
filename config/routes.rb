d82ef7d1c77a69566da00c0ce4fb950b4032ee0e
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-02 13:55:06 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,22 @@ Rails.application.routes.draw do
     resources :items, only: [:index]
   end
 
+  # get '/merchants/items', to: merchants/items#index
+
   resources :items, only: [:index, :show] do
     resources :reviews, only: [:new, :create]
   end
 
+  # get '/items', to: 'items#index'
+  # get '/items/:id', to: 'items#show'
+  # get /items/reviews/new, to: 'items/reviews#new'
+  # post /items/reviews, to: 'items/reviews#create'
+
   resources :reviews, only: [:edit, :update, :destroy]
+
+  # get '/reviews/:id/edit', to: 'reviews#edit'
+  # patch '/reviews/:id', to: 'reviews#update'
+  # delete '/reviews/:id', to: 'reviews#destroy'
 
   get '/cart', to: 'cart#show'
   post '/cart/:item_id', to: 'cart#add_item'
@@ -18,9 +29,14 @@ Rails.application.routes.draw do
   patch '/cart/:change/:item_id', to: 'cart#update_quantity'
   delete '/cart/:item_id', to: 'cart#remove_item'
 
+  # resources :cart, only: [:show, :create, :destroy, :update]
+  # could have cart inheriting multiple controllers for best setup
+
   get '/registration', to: 'users#new', as: :registration
   resources :users, only: [:create, :update]
   patch '/user/:id', to: 'users#update'
+
+  # can create namespace for this as well
   get '/profile', to: 'users#show'
   get '/profile/edit', to: 'users#edit'
   get '/profile/edit_password', to: 'users#edit_password'
@@ -28,6 +44,14 @@ Rails.application.routes.draw do
   get '/profile/orders', to: 'user/orders#index'
   get '/profile/orders/:id', to: 'user/orders#show'
   delete '/profile/orders/:id', to: 'user/orders#cancel'
+  # resources profile do
+  #   resources :orders, only: [:index, :show, :destroy ]
+  # end
+
+  # resources :registration, only: [:new]
+  # resources :users, only: [:new, :create, :update]
+  # resources :orders, only: [:create]
+  # post '/users', to: users#create
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#login'
